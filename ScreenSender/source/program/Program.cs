ae4098e8f206843c29fcbbfd6ebf96ecec71c7ea
awen <quetent@icloud.com>
2023-06-02 23:03:45 +0500

    initial commit

---
+++ ScreenSender/source/program/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ScreenSender
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ ScreenSender/source/program/Settings.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScreenSender;
+
+public static class Settings
+{
+    public static readonly string BotToken;
+
+    static Settings()
+    {
+        BotToken = Secret.BotToken;
+    }
+}
